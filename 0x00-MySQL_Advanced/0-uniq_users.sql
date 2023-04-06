9f78a0ef4413103f66605df1d518963f1468086b
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-06 20:08:43 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,7 @@
 -- id which is integer, never null, auto increment and primary key
 -- email which is string (255 characters), never null and unique
 -- name which is string (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email varchar(255) NOT NULL UNIQUE,
