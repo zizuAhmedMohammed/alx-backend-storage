d47fe5d6b8821ee38a46268ec08070f1c0e73bf6
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-06 19:59:38 +0000

    0-uniq_users.sql

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,5 +1,7 @@
 -- SQL script that creates a table users following these 
--- id, email, name
+-- id which is integer, never null, auto increment and primary key
+-- email which is string (255 characters), never null and unique
+-- name which is string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email varchar(255) NOT NULL UNIQUE,
