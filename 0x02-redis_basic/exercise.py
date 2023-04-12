c7a8a762912344e8ddda4986e72e7089b42fa9c9
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-12 21:59:38 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -75,20 +75,6 @@ class Cache:
         self._redis.flushdb()
 
     
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Stores the input data in Redis using a randomly generated key and returns the key.
-
-        Args:
-            data: The data to be stored in Redis. Can be a str, bytes, int or float.
-
-        Returns:
-            The randomly generated key used to store the data in Redis.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
